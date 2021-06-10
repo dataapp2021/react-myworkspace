574180ee3cb485820eaacef45aef2610aa760c86
KO DAE KEUN <dataapp2021@gmail.com>
2021-06-10 15:00:23 +0900

    데이터 변환 처리 이전

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,6 +5,13 @@ import Hidden from "@material-ui/core/Hidden";
 
 import BarChartSample from "./BarChartSample";
 import LineChartSample from "./LineChartSample";
+import ResponsiveTable from "./ResponsiveTableSample";
+
+import source from "./data/source";
+// import sidoKorName from "./data/sidoKorName";
+import bardata from "./data/bardata";
+import linedata from "./data/linedata";
+import tabledata from "./data/tabledata";
 
 const useStyles = makeStyles((theme) => ({
   // 내부 페이퍼에 스타일을 지정
@@ -22,68 +29,24 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const transformSidoData = (source) => {
+  return bardata;
+};
+
+const transformLocationData = (source, sido) => {
+  return linedata;
+};
+
+const transformSidoTableData = (source) => {
+  return tabledata;
+};
+
 const Home = () => {
   const classes = useStyles();
 
-  const data = [
-    { sido: "seoul", pm10: 20, pm25: 10 },
-    { sido: "gyeonggi", pm10: 20, pm25: 10 },
-    { sido: "incheon", pm10: 14, pm25: 7 },
-    { sido: "gangwon", pm10: 33, pm25: 15 },
-    { sido: "sejong", pm10: 22, pm25: 6 },
-    { sido: "chungbuk", pm10: 34, pm25: 14 },
-    { sido: "chungnam", pm10: 13, pm25: 7 },
-    { sido: "daejeon", pm10: 20, pm25: 7 },
-    { sido: "gyeongbuk", pm10: 23, pm25: 10 },
-    { sido: "gyeongnam", pm10: 17, pm25: 9 },
-    { sido: "daegu", pm10: 21, pm25: 9 },
-    { sido: "ulsan", pm10: 17, pm25: 12 },
-    { sido: "busan", pm10: 19, pm25: 12 },
-    { sido: "jeonbuk", pm10: 9, pm25: 5 },
-    { sido: "jeonnam", pm10: 17, pm25: 9 },
-    { sido: "gwangju", pm10: 12, pm25: 7 },
-    { sido: "jeju", pm10: 10, pm25: 4 },
-  ];
-
-  const sidoKorName = {
-    seoul: "서울",
-    gyeonggi: "경기",
-    incheon: "인천",
-    gangwon: "강원",
-    sejong: "세종",
-    chungbuk: "충북",
-    chungnam: "충남",
-    daejeon: "대전",
-    gyeongbuk: "경북",
-    gyeongnam: "경남",
-    daegu: "대구",
-    ulsan: "울산",
-    busan: "부산",
-    jeonbuk: "전북",
-    jeonnam: "전남",
-    gwangju: "광주",
-    jeju: "제주",
-  };
-
-  for (let elm of data) {
-    elm.sido = sidoKorName[elm.sido];
-  }
-
-  // 서울 중구의 시간대별 변화
-  const locationCurrentData = [
-    { dataTime: "05-27:01", pm10: 46, pm25: 15 },
-    { dataTime: "05-27:02", pm10: 46, pm25: 18 },
-    { dataTime: "05-27:03", pm10: 43, pm25: 16 },
-    { dataTime: "05-27:04", pm10: 37, pm25: 12 },
-    { dataTime: "05-27:05", pm10: 39, pm25: 13 },
-    { dataTime: "05-27:06", pm10: 37, pm25: 14 },
-    { dataTime: "05-27:07", pm10: 38, pm25: 14 },
-    { dataTime: "05-27:08", pm10: 42, pm25: 16 },
-    { dataTime: "05-27:09", pm10: 38, pm25: 15 },
-    { dataTime: "05-27:10", pm10: 22, pm25: 9 },
-    { dataTime: "05-27:11", pm10: 17, pm25: 8 },
-    { dataTime: "05-27:12", pm10: 17, pm25: 10 },
-  ];
+  const sidoData = transformSidoData(source);
+  const locationData = transformLocationData(source, "seoul");
+  const sidoTableData = transformSidoTableData(source.slice(0, 8));
 
   return (
     // Grid 컨테이너 선언
@@ -96,13 +59,15 @@ const Home = () => {
         <Grid item lg={1} />
       </Hidden>
       <Grid item xs={12} sm={7} lg={6}>
-        <Paper className={classes.paper} style={{ height: "20vh" }}>
-          <BarChartSample data={data} />
+        <Paper className={classes.paper} style={{ height: "25vh" }}>
+          <h3>시도별 미세먼지 현재 현황</h3>
+          <BarChartSample data={sidoData} />
         </Paper>
       </Grid>
       <Grid item xs={12} sm={5} lg={4}>
-        <Paper className={classes.paper} style={{ height: "20vh" }}>
-          <LineChartSample data={locationCurrentData} />
+        <Paper className={classes.paper} style={{ height: "25vh" }}>
+          <h3>서울 미세먼지 현황</h3>
+          <LineChartSample data={locationData} />
         </Paper>
       </Grid>
       <Hidden mdDown>
@@ -112,8 +77,9 @@ const Home = () => {
         <Grid item lg={1} />
       </Hidden>
       <Grid item xs={12} sm={12} lg={10}>
-        <Paper className={classes.paper} style={{ height: "40vh" }}>
-          item xs={12} sm={12} lg={10}
+        <Paper className={classes.paper}>
+          <h3>시도별 미세먼지 이력</h3>
+          <ResponsiveTable data={sidoTableData} />
         </Paper>
       </Grid>
       <Hidden mdDown>
---
+++ src/components/home/data/linedata.js
@@ -0,0 +1,64 @@
+const linedata = [
+  {
+    dataTime: "22:00",
+    pm10: 55,
+    pm25: 34,
+  },
+  {
+    dataTime: "23:00",
+    pm10: 54,
+    pm25: 33,
+  },
+  {
+    dataTime: "24:00",
+    pm10: 53,
+    pm25: 32,
+  },
+  {
+    dataTime: "01:00",
+    pm10: 53,
+    pm25: 34,
+  },
+  {
+    dataTime: "02:00",
+    pm10: 55,
+    pm25: 33,
+  },
+  {
+    dataTime: "03:00",
+    pm10: 54,
+    pm25: 33,
+  },
+  {
+    dataTime: "04:00",
+    pm10: 55,
+    pm25: 34,
+  },
+  {
+    dataTime: "05:00",
+    pm10: 57,
+    pm25: 39,
+  },
+  {
+    dataTime: "06:00",
+    pm10: 59,
+    pm25: 39,
+  },
+  {
+    dataTime: "07:00",
+    pm10: 58,
+    pm25: 39,
+  },
+  {
+    dataTime: "08:00",
+    pm10: 52,
+    pm25: 34,
+  },
+  {
+    dataTime: "09:00",
+    pm10: 51,
+    pm25: 32,
+  },
+];
+
+export default linedata;
