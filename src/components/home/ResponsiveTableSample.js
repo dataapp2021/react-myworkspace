8165852e6b8dc2bfc50787fe7a0b0da8c360d6bb
KO DAE KEUN <dataapp2021@gmail.com>
2021-06-17 11:00:24 +0900

    반응형 테이블 제어 변경

--- src/components/home/ResponsiveTableSample.js
+++ src/components/home/ResponsiveTableSample.js
@@ -36,68 +36,68 @@ const ResponsiveTable = ({ data }) => {
 
   return (
     // <Table >
-
-    <Table style={{ borderCollapse: "collapse" }}>
-      <Thead>
-        <Tr>
-          {data.length > 0 &&
-            Object.keys(data[0]).map((key) => (
+    data.length > 0 && (
+      <Table style={{ borderCollapse: "collapse" }}>
+        <Thead>
+          <Tr>
+            {Object.keys(data[0]).map((key, index) => (
               <Th
                 style={{
                   borderBottom: "1px solid rgba(224, 224, 224)",
                   lineHeight: "2rem",
                   fontWeight: "bold",
                 }}
-                key={`th-${key}`}
+                key={`th-${index}`}
               >
                 {key}
               </Th>
             ))}
-        </Tr>
-      </Thead>
-      <Tbody>
-        {data.map((item) => (
-          <Tr
-            key={`tr-${item.시간}-${item.구분}`}
-            style={{
-              border: "0px",
-              borderBottom: "1px solid rgba(224, 224, 224)",
-            }}
-          >
-            {Object.keys(item).map((key) => (
-              <Td
-                style={{
-                  borderBottom: "1px solid rgba(224, 224, 224)",
-                  lineHeight: "2rem",
-                }}
-                key={`td-${key}`}
-              >
-                {["시간", "구분"].indexOf(key) > -1 ? (
-                  <span
-                    style={{
-                      color: item.구분 === "PM10" ? "#8884d8" : "#82ca9d",
-                    }}
-                  >
-                    {item[key]}
-                  </span>
-                ) : (
-                  <span
-                    style={{
-                      color:
-                        item.구분 === "PM10"
-                          ? pm10Color(item[key])
-                          : pm25Color(item[key]),
-                    }}
-                  >
-                    {item[key]}
-                  </span>
-                )}
-              </Td>
-            ))}
           </Tr>
-        ))}
-      </Tbody>
-    </Table>
+        </Thead>
+        <Tbody>
+          {data.map((item) => (
+            <Tr
+              key={`tr-${item.시간}-${item.구분}`}
+              style={{
+                border: "0px",
+                borderBottom: "1px solid rgba(224, 224, 224)",
+              }}
+            >
+              {Object.keys(item).map((key, index) => (
+                <Td
+                  style={{
+                    borderBottom: "1px solid rgba(224, 224, 224)",
+                    lineHeight: "2rem",
+                  }}
+                  key={`td-${index}`}
+                >
+                  {["시간", "구분"].indexOf(key) > -1 ? (
+                    <span
+                      style={{
+                        color: item.구분 === "PM10" ? "#8884d8" : "#82ca9d",
+                      }}
+                    >
+                      {item[key]}
+                    </span>
+                  ) : (
+                    <span
+                      style={{
+                        color:
+                          item.구분 === "PM10"
+                            ? pm10Color(item[key])
+                            : pm25Color(item[key]),
+                      }}
+                    >
+                      {item[key]}
+                    </span>
+                  )}
+                </Td>
+              ))}
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    )
   );
 };
 
